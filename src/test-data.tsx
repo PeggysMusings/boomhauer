96b28452db23b3102520f1bf370b65e9f0adc24d
Tyler N <tyler.nally11@gmail.com>
2023-08-23 18:47:33 -0500

    fix: correct file types

---
+++ src/test-data.tsx
@@ -0,0 +1,88 @@
+import sample_img from './assets/sample_img.jpg';
+
+export const testData = [
+    {
+        "id":1,
+        "episode":"S1E1",
+        "timestamp":297339,
+        "quote": "Test quote 123",
+        "url": sample_img
+    },
+    {
+        "id":2,
+        "episode":"S1E2",
+        "timestamp":1250166,
+        "quote": "Test quote 321",
+        "url": sample_img
+    },
+    {
+        "id":3,
+        "episode":"S1E3",
+        "timestamp":1250136,
+        "quote": "Test quote 456",
+        "url": sample_img
+    },
+    {
+        "id":4,
+        "episode":"S1E2",
+        "timestamp":125776,
+        "quote": "Test quote 654",
+        "url": sample_img
+    },
+    {
+        "id":5,
+        "episode":"S1E3",
+        "timestamp":125236,
+        "quote": "Test quote 789",
+        "url": sample_img
+    },
+    {
+        "id":6,
+        "episode":"S1E2",
+        "timestamp":1230166,
+        "quote": "Test quote 987",
+        "url": sample_img
+    },
+    {
+        "id":7,
+        "episode":"S1E3",
+        "timestamp":114166,
+        "quote": "Test quote 10",
+        "url": sample_img
+    },
+    {
+        "id":8,
+        "episode":"S1E3",
+        "timestamp":550166,
+        "quote": "Test quote 20",
+        "url": sample_img
+    },
+    {
+        "id":9,
+        "episode":"S1E3",
+        "timestamp":70166,
+        "quote": "Test quote 30",
+        "url": sample_img
+    },
+    {
+        "id":10,
+        "episode":"S1E3",
+        "timestamp":6650166,
+        "quote": "Test quote 40",
+        "url": sample_img
+    },
+    {
+        "id":11,
+        "episode":"S1E3",
+        "timestamp":1254666,
+        "quote": "Test quote 50",
+        "url": sample_img
+    },
+    {
+        "id":12,
+        "episode":"S1E3",
+        "timestamp":1242166,
+        "quote": "Test quote 60",
+        "url": sample_img
+    }
+];
--- src/types/gql.types.ts
+++ src/types/gql.types.ts
@@ -1,15 +1,15 @@
 const { gql } = require('@apollo/client');
 
-export const gqlTypes = gql`
-    type Query {
-        selectedImg: [SelectedImg!]!
-    }
-    
+export const gqlTypes = gql`    
     type SelectedImg {
         id: Int!
         episode: String!
         timestamp: Int!
         quote: String!
         url: String!
+    }
+
+    type Query {
+        selectedImg: [SelectedImg!]!
     }
 `
